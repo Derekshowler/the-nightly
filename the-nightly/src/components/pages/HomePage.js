4a0e8f15ef775c5aedc4ab085d5845f24b92186e
Showler <derekshowler@gmail.com>
2020-04-10 20:06:06 -0400

    Nightly-1-fix-relative-paths
    Fix paths for react router dom

--- the-nightly/src/components/pages/HomePage.js
+++ the-nightly/src/components/pages/HomePage.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 
 const HomePage = () => (
     <div>
-        <h1>Home Page</h1>
+        <h1>Welcome to The-Nightly</h1>
         <Link to = "/login">Login</Link>
     </div>
 );
