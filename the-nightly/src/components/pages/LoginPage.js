cf71c2417c22435368d7a84ccb5153508e1d6db2
Showler <derekshowler@gmail.com>
2020-04-11 09:35:14 -0400

    Nightly-3-Login
    Submit data on login and return errors based on status of form fields

--- the-nightly/src/components/pages/LoginPage.js
+++ the-nightly/src/components/pages/LoginPage.js
@@ -1,10 +1,21 @@
 import React from "react";
+import LoginForm from "../forms/LoginForm";
+import { render } from "react-dom";
 
+class LoginPage extends React.Component {
+    submit = data => {
+        console.log(data);
+};
 
-const LoginPage = () => (
-    <div>
-        <h1>Login page</h1>
-    </div>
-);
+render() {
+    return (
+       <div>
+            <h1>Login page</h1>
+
+            <LoginForm submit = {this.submit}/>
+        </div>
+        );
+    }
+}
 
 export default LoginPage;
