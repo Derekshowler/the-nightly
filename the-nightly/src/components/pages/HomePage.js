6086c8328f450c702598a64c576842826c2b470e
Showler <derekshowler@gmail.com>
2020-04-23 22:30:23 -0400

    Nightly-7
    More homepage styling / layering

--- the-nightly/src/components/pages/HomePage.js
+++ the-nightly/src/components/pages/HomePage.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import { 
-    HomePageStyle,
-    HomePageTitle
+    HomePageContainer,
+    HomePageFeaturedTitle,
+    HomePageFeaturedContainer,
+    HomePageFeaturedContent,
+    HomePageFeaturedList
 } from '../styles/homePageStyle';
 
 
 export default function HomePage() {
     return (
-        <HomePageStyle>
-            <div>
-                <HomePageTitle>
-                    Homepage: Content
-                </HomePageTitle>
-            </div>
-        </HomePageStyle>
+        <>
+        <h1>The night is thriving</h1>
+        <HomePageContainer>
+                <HomePageFeaturedContainer>
+                        <HomePageFeaturedTitle>
+
+                        </HomePageFeaturedTitle>
+
+                        <HomePageFeaturedContent>
+
+                        </HomePageFeaturedContent>
+
+                </HomePageFeaturedContainer>
+
+                <HomePageFeaturedContainer>
+                        <HomePageFeaturedTitle>
+                            
+                        </HomePageFeaturedTitle>
+
+                        <HomePageFeaturedContent>
+                            
+                        </HomePageFeaturedContent>
+
+                </HomePageFeaturedContainer>
+
+                <HomePageFeaturedContainer>
+                        <HomePageFeaturedTitle>
+                            
+                        </HomePageFeaturedTitle>
+
+                        <HomePageFeaturedContent>
+                            
+                        </HomePageFeaturedContent>
+
+                        <HomePageFeaturedList>
+                            <p>some details</p>
+                        </HomePageFeaturedList>
+                </HomePageFeaturedContainer>
+        </HomePageContainer>
+        </>
     );
 }
 
--- the-nightly/src/components/styles/homePageStyle.js
+++ the-nightly/src/components/styles/homePageStyle.js
@@ -1,15 +1,41 @@
-import styled, { css } from 'styled-components';
+import styled, { css, isStyledComponent } from 'styled-components';
 import { Link } from 'react-router-dom';
 
 //main navigation bar
-export const HomePageStyle = styled.div`
-    display: flex;
-    padding: 20px;
-    align-items: center;
-    margin-bottom: 40px;
-    background-color: #006699;
+export const HomePageContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+    padding: 10px;
+    background-color: #111111;
 `;
 
-export const HomePageTitle = styled.h2`
-  font-size: 35px;
+export const HomePageFeaturedTitle = styled.h2`
+    text-align: center;
+    font-weight: 300;
+`;
+
+export const HomePageFeaturedContainer = styled.div`
+    background: white;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 1px 1px 5px #ccc;
+    text-align: center;
+
+    &:hover {
+        box-shadow: 3px 3px 5px #ccc;
+        transform: translateY(-2px);
+    }
+`;
+
+export const HomePageFeaturedList = styled.ul`
+    text-align: left;
+    margin-bottom: 60px;
+`;
+
+export const HomePageFeaturedContent = styled.span`
+    text-align: center;
+    font-size: 54px;
+    margin-top: 30px;
+    margin-bottom: 60px;
 `;
